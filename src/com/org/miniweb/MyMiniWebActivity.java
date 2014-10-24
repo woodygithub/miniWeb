fae8d45b7a2374d4399c2ea259ce1e1196d81cb5
woodygithub <woody.fire@aliyun.con>
2014-10-24 14:00:58 +0800

    new my miniweb activity

---
+++ src/com/org/miniweb/MyMiniWebActivity.java
@@ -0,0 +1,14 @@
+package com.org.miniweb;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MyMiniWebActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_mini_web_activity);
+    }
+}
